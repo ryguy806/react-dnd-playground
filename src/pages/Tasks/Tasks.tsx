626063435b5d2f3d87c2a933add562e3c9e59c69
Ryan Guelzo <r.guelzo88@gmail.com>
2024-07-26 10:26:25 -0700

    Finished Task Page.

--- src/pages/Tasks/Tasks.tsx
+++ src/pages/Tasks/Tasks.tsx
@@ -1,39 +1,52 @@
+import { MoonIcon, SunIcon } from "@chakra-ui/icons";
 import {
   Box,
   Divider,
   Grid,
+  IconButton,
   Stack,
   Text,
   useColorMode,
 } from "@chakra-ui/react";
 
 const Tasks = () => {
-  const { colorMode } = useColorMode();
+  const { colorMode, toggleColorMode } = useColorMode();
   const wipBG = { light: "teal.300", dark: "teal.600" };
   const doneBG = { light: "blue.300", dark: "blue.600" };
   return (
-    <Grid gap={6} templateColumns='repeat(2, 1fr)' w='80vw' h='93vh'>
-      <Box bg={wipBG[colorMode]} rounded={"md"} p={3} boxShadow={"md"}>
-        <Stack spacing={3}>
-          <Box>
-            <Text fontSize={"2xl"} textAlign={"center"}>
-              WIP Tasks
-            </Text>
-            <Divider />
-          </Box>
-        </Stack>
-      </Box>
-      <Box bg={doneBG[colorMode]} rounded={"md"} p={3} boxShadow={"md"}>
-        <Stack spacing={3}>
-          <Box>
-            <Text fontSize={"2xl"} textAlign={"center"}>
-              Done Tasks
-            </Text>
-            <Divider />
-          </Box>
-        </Stack>
-      </Box>
-    </Grid>
+    <div>
+      <div>
+        <IconButton
+          aria-label='color mode swithc'
+          icon={colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+          onClick={toggleColorMode}
+        >
+          Change Color Mode
+        </IconButton>
+      </div>
+      <Grid gap={6} templateColumns='repeat(2, 1fr)' w='80vw' h='93vh'>
+        <Box bg={wipBG[colorMode]} rounded={"md"} p={3} boxShadow={"md"}>
+          <Stack spacing={3}>
+            <Box>
+              <Text fontSize={"2xl"} textAlign={"center"}>
+                WIP Tasks
+              </Text>
+              <Divider />
+            </Box>
+          </Stack>
+        </Box>
+        <Box bg={doneBG[colorMode]} rounded={"md"} p={3} boxShadow={"md"}>
+          <Stack spacing={3}>
+            <Box>
+              <Text fontSize={"2xl"} textAlign={"center"}>
+                Done Tasks
+              </Text>
+              <Divider />
+            </Box>
+          </Stack>
+        </Box>
+      </Grid>
+    </div>
   );
 };
 
