a84a19633bced29f1afaacbb20d722f9bfc53fca
Ryan Guelzo <r.guelzo88@gmail.com>
2024-07-26 11:22:15 -0700

    Created Draggable Item, starting Drop Target.

---
+++ src/components/BoxTarget.tsx
@@ -0,0 +1,5 @@
+const BoxTarget = () => {
+  return <div>BoxTarget</div>;
+};
+
+export default BoxTarget;
--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,4 +1,6 @@
 import { Box, Flex, Badge, Text } from "@chakra-ui/react";
+import { ItemTypes } from "../utils/ItemTypes";
+import { useDrag } from "react-dnd";
 
 type TaskCardProps = {
   title: string;
@@ -7,8 +9,21 @@ type TaskCardProps = {
 };
 
 const TaskCard = ({ title, category, description }: TaskCardProps) => {
+  const [{ isDragging }, drag] = useDrag({
+    type: ItemTypes.CARD,
+    collect: (monitor) => ({
+      isDragging: !!monitor.isDragging,
+    }),
+  });
   return (
-    <Box bg='orange.300' p={3} boxShadow={"sm"} mt='3'>
+    <Box
+      ref={drag}
+      opacity={!isDragging ? 0.5 : 1}
+      bg='orange.300'
+      p={3}
+      boxShadow={"sm"}
+      mt='3'
+    >
       <Flex direction='row' justify={"space-between"} mb={3}>
         <Text>{title}</Text>
         <Badge colorScheme='cyan'>{category}</Badge>
