962f486ddd0751f8ff6b12b90f84f2dd80a5f318
Ryan Guelzo <r.guelzo88@gmail.com>
2024-07-26 11:11:21 -0700

    Added in Dnd Provider

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,22 @@ import {
 } from "@chakra-ui/react";
 import "./App.css";
 import Tasks from "./pages/Tasks/Tasks";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 
 function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
         <ColorModeProvider>
-          <Flex direction='column' align='center' justify='center'>
-            <CSSReset />
-            <Flex justify='center' align='center'>
-              <Tasks />
+          <DndProvider backend={HTML5Backend}>
+            <Flex direction='column' align='center' justify='center'>
+              <CSSReset />
+              <Flex justify='center' align='center'>
+                <Tasks />
+              </Flex>
             </Flex>
-          </Flex>
+          </DndProvider>
         </ColorModeProvider>
       </ThemeProvider>
     </>
