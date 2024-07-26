1433b707a587fb4f511f5a676a2278e8691e0a35
Ryan Guelzo <r.guelzo88@gmail.com>
2024-07-26 11:05:12 -0700

    Ready for DnD.

--- src/pages/Tasks/Tasks.tsx
+++ src/pages/Tasks/Tasks.tsx
@@ -9,31 +9,39 @@ import {
   useColorMode,
 } from "@chakra-ui/react";
 import TaskCard from "../../components/TaskCard";
-
-const TaskCards = [
-  {
-    title: "Task 1",
-    category: "Category 1",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-  },
-  {
-    title: "Task 2",
-    category: "Category 2",
-    description:
-      "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-  },
-  {
-    title: "Task 3",
-    category: "Category 3",
-    description:
-      "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-  },
-];
+import { useState } from "react";
 
 const Tasks = () => {
   const { colorMode, toggleColorMode } = useColorMode();
   const wipBG = { light: "teal.300", dark: "teal.600" };
   const doneBG = { light: "blue.300", dark: "blue.600" };
+
+  const [taskList, setTaskList] = useState([
+    {
+      id: (Math.random() * 100).toFixed(0),
+      status: "wip",
+      title: "Task 1",
+      category: "Category 1",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+    {
+      id: (Math.random() * 100).toFixed(0),
+      status: "done",
+      title: "Task 2",
+      category: "Category 2",
+      description:
+        "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    },
+    {
+      id: (Math.random() * 100).toFixed(0),
+      status: "wip",
+      title: "Task 3",
+      category: "Category 3",
+      description:
+        "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    },
+  ]);
+
   return (
     <div>
       <div>
@@ -55,15 +63,17 @@ const Tasks = () => {
               <Divider />
             </Box>
           </Stack>
-          {TaskCards.map((card, i) => (
-            <div key={i}>
-              <TaskCard
-                title={card.title}
-                category={card.category}
-                description={card.description}
-              />
-            </div>
-          ))}
+          {taskList
+            .filter((task) => task.status === "wip")
+            .map((card) => (
+              <div key={card.id}>
+                <TaskCard
+                  title={card.title}
+                  category={card.category}
+                  description={card.description}
+                />
+              </div>
+            ))}
         </Box>
         <Box bg={doneBG[colorMode]} rounded={"md"} p={3} boxShadow={"md"}>
           <Stack spacing={3}>
@@ -74,6 +84,17 @@ const Tasks = () => {
               <Divider />
             </Box>
           </Stack>
+          {taskList
+            .filter((task) => task.status === "done")
+            .map((card) => (
+              <div key={card.id}>
+                <TaskCard
+                  title={card.title}
+                  category={card.category}
+                  description={card.description}
+                />
+              </div>
+            ))}
         </Box>
       </Grid>
     </div>
